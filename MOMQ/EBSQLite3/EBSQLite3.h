895a7a2a3b3f933230f7eb0c981314f988ffc165
Adrian Cruz <Adrian_Cruz@cable.comcast.com>
2017-05-15 09:57:32 -0400

    removed sqlite3 dependency

---
+++ MOMQ/EBSQLite3/EBSQLite3.h
@@ -0,0 +1,20 @@
+//
+//  EBSQLite3.h
+//  EBSQLite3
+//
+//  Created by Cruz, Adrian (Contractor) on 5/11/17.
+//  Copyright © 2017 galblank. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for EBSQLite3.
+FOUNDATION_EXPORT double EBSQLite3VersionNumber;
+
+//! Project version string for EBSQLite3.
+FOUNDATION_EXPORT const unsigned char EBSQLite3VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EBSQLite3/PublicHeader.h>
+
+//#import <EBSQLite3/EBSQLite3MiddleMan.h>
+#import <EBSQLite3/sqlite3.h>
